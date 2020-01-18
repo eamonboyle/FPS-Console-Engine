41ae6bce2ab72b71fa810bdca6e97cf5bd7766c8
eamonboyle <blaowskate@hotmail.com>
2020-01-18 00:09:03 +0000

    Completed maze movement and collision.

--- FPS Console Engine/FPS Console Engine.cpp
+++ FPS Console Engine/FPS Console Engine.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <Windows.h>
 #include <chrono>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -34,15 +36,15 @@ int main()
     map += L"#..............#";
     map += L"#..............#";
     map += L"#..............#";
+    map += L"#..........#...#";
+    map += L"#..........#...#";
     map += L"#..............#";
     map += L"#..............#";
     map += L"#..............#";
     map += L"#..............#";
     map += L"#..............#";
     map += L"#..............#";
-    map += L"#..............#";
-    map += L"#..............#";
-    map += L"#..............#";
+    map += L"#.......########";
     map += L"#..............#";
     map += L"#..............#";
     map += L"################";
@@ -63,11 +65,35 @@ int main()
         // handle counter clockwise rotation
         if (GetAsyncKeyState((unsigned short)'A') & 0x8000)
         {
-            playerA -= (0.1f) * elapsedTime;
+            playerA -= (0.8f) * elapsedTime;
         }
         if (GetAsyncKeyState((unsigned short)'D') & 0x8000)
         {
-            playerA += (0.1f) * elapsedTime;
+            playerA += (0.8f) * elapsedTime;
+        }
+
+        if (GetAsyncKeyState((unsigned short)'W') & 0x8000)
+        {
+            playerX += sinf(playerA) * 5.0f * elapsedTime;
+            playerY += cosf(playerA) * 5.0f * elapsedTime;
+
+            if (map[(int)playerY * mapWidth + (int)playerX] == '#')
+            {
+                playerX -= sinf(playerA) * 5.0f * elapsedTime;
+                playerY -= cosf(playerA) * 5.0f * elapsedTime;
+            }
+        }
+
+        if (GetAsyncKeyState((unsigned short)'S') & 0x8000)
+        {
+            playerX -= sinf(playerA) * 5.0f * elapsedTime;
+            playerY -= cosf(playerA) * 5.0f * elapsedTime;
+
+            if (map[(int)playerY * mapWidth + (int)playerX] == '#')
+            {
+                playerX += sinf(playerA) * 5.0f * elapsedTime;
+                playerY += cosf(playerA) * 5.0f * elapsedTime;
+            }
         }
 
         for (int x = 0; x < screenWidth; x++)
@@ -75,8 +101,10 @@ int main()
             // for each column, calculate the projected ray angle into world space
             float rayAngle = (playerA - fov / 2.0f) + ((float)x / (float)screenWidth) * fov;
 
-            float distanceToWall = 0;
+            float distanceToWall = 0.0f;
+
             bool hitWall = false;
+            bool boundary = false;
 
             float eyeX = sinf(rayAngle);
             float eyeY = cosf(rayAngle);
@@ -100,6 +128,28 @@ int main()
                     if (map[testY * mapWidth + testX] == '#')
                     {
                         hitWall = true;
+
+                        vector<pair<float, float>> p; // distance, dot
+
+                        for (int tx = 0; tx < 2; tx++)
+                        {
+                            for (int ty = 0; ty < 2; ty++)
+                            {
+                                float vy = (float)testY + ty - playerY;
+                                float vx = (float)testX + tx - playerX;
+                                float d = sqrt(vx * vx + vy * vy);
+                                float dot = (eyeX * vx / d) + (eyeY * vy / d);
+                                p.push_back(make_pair(d, dot));
+                            }
+                        }
+
+                        // sort pairs from closest to farthest
+                        sort(p.begin(), p.end(), [](const pair<float, float>& left, const pair<float, float> &right) { return left.first < right.first; });
+
+                        float bound = 0.01f;
+                        if (acos(p.at(0).second) < bound) boundary = true;
+                        if (acos(p.at(1).second) < bound) boundary = true;
+                        //if (acos(p.at(2).second) < bound) boundary = true;
                     }
                 }
             }
@@ -108,22 +158,59 @@ int main()
             int ceiling = (float)(screenHeight / 2.0) - screenHeight / ((float)distanceToWall);
             int floor = screenHeight - ceiling;
 
+            // calculate wall distance
+            short shade = ' ';
+
+            if (distanceToWall <= depth / 4.0f)         shade = 0x2588; // close
+            else if (distanceToWall < depth / 3.0f)     shade = 0x2593;
+            else if (distanceToWall < depth / 2.0f)     shade = 0x2592;
+            else if (distanceToWall < depth)            shade = 0x2591;
+            else                                        shade = ' ';    // far away
+
+            // shade boundary
+            if (boundary)
+            {
+                shade = ' ';
+            }
+
             for (int y = 0; y < screenHeight; y++)
             {
-                if (y < ceiling)
+                if (y <= ceiling)
                 {
                     screen[y * screenWidth + x] = ' ';
                 }
                 else if (y > ceiling&& y <= floor)
                 {
-                    screen[y * screenWidth + x] = '#';
+                    screen[y * screenWidth + x] = shade;
                 }
                 else
                 {
-                    screen[y * screenWidth + x] = ' ';
+                    float b = 1.0f - (((float)y - screenHeight / 2.0f) / ((float)screenHeight / 2.0f));
+                    if (b < 0.25) shade = '#';
+                    else if (b < 0.25) shade = 'x';
+                    else if (b < 0.75) shade = '.';
+                    else if (b < 0.9) shade = '-';
+                    else shade = ' ';
+
+                    screen[y * screenWidth + x] = shade;
                 }
             }
         }
+
+        // display stats
+        swprintf_s(screen, 40, L"X=%3.2f, Y=%3.2f, A=%3.2f FPS=%3.2f ", playerX, playerY, playerA, 1.0f / elapsedTime);
+
+        // display map
+        for (int nx = 0; nx < mapWidth; nx++)
+        {
+            for (int ny = 0; ny < mapWidth; ny++)
+            {
+                screen[(ny + 1) * screenWidth + nx] = map[ny * mapWidth + nx];
+            }
+        }
+
+        // show player on map
+        screen[((int)playerY + 1) * screenWidth + (int)playerX] = 'P';
 
         screen[screenWidth * screenHeight - 1] = '\0';
         WriteConsoleOutputCharacter(hConsole, screen, screenWidth * screenHeight, { 0,0 }, &bytesWritten);
